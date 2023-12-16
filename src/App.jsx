698e118f55d231cbc587a668cbada78ee8fedffe
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-16 23:09:22 +0530

    Add: Animation to simulate movement of elements

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
           return (
             <div
               key={index}
-              className={`rounded-lg font-[75px] h-32 w-24 m-[1%] text-center text-white focus:outline-none pt-11 px-3 overflow-hidden bg-[#1e2d2f] ${sorted ? 'border border-solid border-green-500 element-transition' : 'border border-solid border-red-500 element-transition'} transition-all duration-300 m-4`}
+              className={`rounded-lg font-[75px] h-32 w-24 m-[1%] text-center text-white focus:outline-none pt-11 px-3 overflow-hidden bg-[#1e2d2f] ${sorted ? 'border border-solid border-green-500 element-transition-sort' : 'border border-solid border-red-500 element-transition-sort'} transition-all duration-300 m-4`}
             >
               {element}
             </div>
