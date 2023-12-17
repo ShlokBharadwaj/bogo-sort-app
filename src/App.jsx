020731ffdc4cf29954d8360646a377463b6f3e0c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-12-17 19:03:19 +0530

    Add: Number of iterations

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [inputArray, setInputArray] = useState([]);
   const [sorted, setSorted] = useState(false);
+  const [iterations, setIterations] = useState(0);
 
   useEffect(() => {
     let sortInterval;
@@ -30,6 +31,7 @@ function App() {
         const sortedArray = shuffleArray(newArray);
         setInputArray(sortedArray);
         setSorted(isSorted(sortedArray));
+        setIterations(prevIterations => prevIterations + 1);
       };
 
       sortInterval = setInterval(sortArray, 1000);
@@ -51,7 +53,8 @@ function App() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#041f1e] text-white font-pacifico">
       <div className="text-4xl mb-4">bogo sort app</div>
       <div className='w-3/4 flex flex-col items-center justify-center m-0'>
-        <h3 className='text-white mt-3 mb-5 text-center'>Enter all the numbers separated by space(' ').
+        <h3 className='text-white mt-3 mb-5 text-center'>Enter all the numbers separated by space(' ').<br />
+          {sorted && iterations > 1 ? `Array sorted in ${iterations-1} iterations` : ''}
         </h3>
         <textarea
           className="rounded-lg font-[75px] h-32 w-24 border-solid border-[#eee] m-[1%] text-center text-white focus:outline-none pt-11 px-3 overflow-hidden bg-[#1e2d2f] resize-none mb-10"
